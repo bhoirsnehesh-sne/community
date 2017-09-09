673b3e8b59d3540cb305fd4add2d37c53cebc0b6
bhoirsnehesh-sne <bhoirsnehesh@gmail.com>
2017-09-09 19:02:24 +0530

    Create action.php

---
+++ action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Try v1.2 Bootstrap Online</title>
+   <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+   <script src="/scripts/jquery.min.js"></script>
+   <script src="/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class = "alert alert-success">Success! Well done its submitted.</div>
+
+</body>
+</html>
